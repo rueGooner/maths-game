e2831c5b935a03da683f3300e6c48604e256e9fc
Rueben <ruebencee@gmail.com>
2020-01-28 21:17:39 +0000

    add buefy

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,45 @@
 </template>
 
 <style lang="scss">
+
+// Import Bulma's core
+@import "~bulma/sass/utilities/_all";
+
+// Set your colors
+$primary: #8c67ef;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099FF;
+$twitter-invert: findColorInvert($twitter);
+
+// Setup $colors to use as bulma classes (e.g. 'is-twitter')
+$colors: (
+    "white": ($white, $black),
+    "black": ($black, $white),
+    "light": ($light, $light-invert),
+    "dark": ($dark, $dark-invert),
+    "primary": ($primary, $primary-invert),
+    "info": ($info, $info-invert),
+    "success": ($success, $success-invert),
+    "warning": ($warning, $warning-invert),
+    "danger": ($danger, $danger-invert),
+    "twitter": ($twitter, $twitter-invert)
+);
+
+// Links
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+
+// Import Bulma and Buefy styles
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $primary;
 }
 
 #nav {
@@ -22,10 +55,10 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $primary;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $twitter;
     }
   }
 }
